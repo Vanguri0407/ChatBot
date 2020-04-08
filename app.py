d43c561420acfa61b109bd322c98637aa0558cd0
Vanguri0407 <49231611+Vanguri0407@users.noreply.github.com>
2020-04-08 14:47:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,32 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
+def predictsalary():
+    '''
+    For rendering results to DialogFlow
+    '''
+    sessionID=req.get('responseId')
+
+
+    result = req.get("queryResult")
+    user_says=result.get("queryText")
+    log.write_log(sessionID, "User Says: "+user_says)
+    parameters = result.get("parameters")
+    interview_score=parameters.get("interview_score")
+    #print(cust_name)
+    test_score = parameters.get("test_score")
+    experience=parameters.get("experience")
+    #course_name= parameters.get("course_name")  
+    
+    int_features = [interview_score,test_score,experience]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    fulfillmentText = output
+    return {
+            "fulfillmentText": fulfillmentText
+        }
 
 if __name__ == "__main__":
     app.run(debug=True)
