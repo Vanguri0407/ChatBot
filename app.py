be6c79287682834996d4a909453978eaa9ccb96f
Shiva Prasad Reddy V <49231611+Vanguri0407@users.noreply.github.com>
2020-05-11 23:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,11 +81,9 @@ if(str =='chatbot_app'):
     net = tflearn.regression(net)
     model = tflearn.DNN(net)
 
-    try:
-        model.load("model.tflearn")
-    except:
-        model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-        model.save("model.tflearn")
+
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
 
 
     def bag_of_words(s, words):
